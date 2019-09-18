7889b2f34a3874851a161c0d82dd7ba887b52004
Stefan Vasic <stefan_94@rocketmail.com>
2019-09-18 13:06:15 +0200

    BookingApp - Controllers added for Place and Country

---
+++ BookingApp/Controllers/PlacesController.cs
@@ -0,0 +1,124 @@
+﻿using BookingApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+	public class PlacesController : ApiController
+	{
+		private IBAContext db;
+
+		public PlacesController()
+		{
+			db = new BAContext();
+		}
+
+		public PlacesController(IBAContext context)
+		{
+			db = context;
+		}
+
+		// GET: api/Places
+		public IQueryable<Place> GetPlaces()
+		{
+			return db.Places;
+		}
+
+		// GET: api/Places/5
+		[ResponseType(typeof(Place))]
+		public IHttpActionResult GetPlace(int id)
+		{
+			Place place = db.Places.Find(id);
+			if (place == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(place);
+		}
+
+		// PUT: api/Places/5
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PutPlace(int id, Place place)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (id != place.Id)
+			{
+				return BadRequest();
+			}
+
+			db.MarkAsModified(place);
+
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!PlaceExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return StatusCode(HttpStatusCode.NoContent);
+		}
+
+		// POST: api/Places
+		[ResponseType(typeof(Place))]
+		public IHttpActionResult PostPlace(Place place)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			db.Places.Add(place);
+			db.SaveChanges();
+
+			return CreatedAtRoute("DefaultApi", new { id = place.Id }, place);
+		}
+
+		// DELETE: api/Places/5
+		[ResponseType(typeof(Place))]
+		public IHttpActionResult DeletePlace(int id)
+		{
+			Place place = db.Places.Find(id);
+			if (place == null)
+			{
+				return NotFound();
+			}
+
+			db.Places.Remove(place);
+			db.SaveChanges();
+
+			return Ok(place);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private bool PlaceExists(int id)
+		{
+			return db.Places.Count(e => e.Id == id) > 0;
+		}
+	}
+}
