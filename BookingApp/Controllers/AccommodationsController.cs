75a4fe47c3fae3d4213c7ae38b6e6b56da82811b
Stefan Vasic <stefan_94@rocketmail.com>
2019-11-04 14:51:12 +0100

    Update AccommodationsController.cs

--- BookingApp/Controllers/AccommodationsController.cs
+++ BookingApp/Controllers/AccommodationsController.cs
@@ -8,127 +8,134 @@ using System.Web.Http.Description;
 
 namespace BookingApp.Controllers
 {
-    public class AccommodationsController : ApiController
-    {
-        private IBAContext db;
+	public class AccommodationsController : ApiController
+	{
+		private IBAContext db;
 
-        public AccommodationsController()
-        {
-            db = new BAContext();
-        }
+		public AccommodationsController()
+		{
+			db = new BAContext();
+		}
 
-        public AccommodationsController(IBAContext context)
-        {
-            db = context;
-        }
+		public AccommodationsController(IBAContext context)
+		{
+			db = context;
+		}
 
-        // GET: api/Accommodations
-        public IQueryable<Accommodation> GetAccommodations()
-        {
-            return db.Accommodations.Include(x => x.AccommodationType).Include(p => p.Place).Include(t => t.Rooms);
-        }
+		// GET: api/Accommodations
+		public IQueryable<Accommodation> GetAccommodations()
+		{
+			return db.Accommodations.Include(x => x.AccommodationType).Include(p => p.Place).Include(t => t.Rooms);
+		}
 
-        // GET: api/Accommodations/5
-        [ResponseType(typeof(Accommodation))]
-        public IHttpActionResult GetAccommodation(int id)
-        {
-            Accommodation accommodation = db.Accommodations.Find(id);
-            if (accommodation == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(accommodation);
-        }
-
-        // PUT: api/Accommodations/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutAccommodation(int id, Accommodation accommodation)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != accommodation.Id)
-            {
-                return BadRequest();
-            }
-
-            db.MarkAsModified(accommodation);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AccommodationExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Accommodations
-        [ResponseType(typeof(Accommodation))]
-        public IHttpActionResult PostAccommodation(Accommodation accommodation)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (AccommodationExists(accommodation.Id))
-            {
-                return BadRequest("Accommodation exists.");
-            }
+		// GET: api/Accommodations
+		public IQueryable<Accommodation> GetPopularAccommodations()
+		{
+			return db.Accommodations.Include(x => x.AccommodationType).Include(p => p.Place).Include(t => t.Rooms).Where(x=>x.AverageGrade>4);
+		}
 
 
-            accommodation.Place = db.Places.Find(accommodation.Place.Id);
-            accommodation.AccommodationType = db.AccommodationTypes.Find(accommodation.AccommodationType.Id);
-            accommodation.Comments = new System.Collections.Generic.List<Comment>();
+		// GET: api/Accommodations/5
+		[ResponseType(typeof(Accommodation))]
+		public IHttpActionResult GetAccommodation(int id)
+		{
+			Accommodation accommodation = db.Accommodations.Find(id);
+			if (accommodation == null)
+			{
+				return NotFound();
+			}
 
-            db.Accommodations.Add(accommodation);
-            db.SaveChanges();
+			return Ok(accommodation);
+		}
 
-            return CreatedAtRoute("DefaultApi", new { id = accommodation.Id }, accommodation);
-        }
+		// PUT: api/Accommodations/5
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PutAccommodation(int id, Accommodation accommodation)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
-        // DELETE: api/Accommodations/5
-        [ResponseType(typeof(Accommodation))]
-        public IHttpActionResult DeleteAccommodation(int id)
-        {
-            Accommodation accommodation = db.Accommodations.Find(id);
-            if (accommodation == null)
-            {
-                return NotFound();
-            }
+			if (id != accommodation.Id)
+			{
+				return BadRequest();
+			}
 
-            db.Accommodations.Remove(accommodation);
-            db.SaveChanges();
+			db.MarkAsModified(accommodation);
 
-            return Ok(accommodation);
-        }
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!AccommodationExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+			return StatusCode(HttpStatusCode.NoContent);
+		}
 
-        private bool AccommodationExists(int id)
-        {
-            return db.Accommodations.Count(e => e.Id == id) > 0;
-        }
-    }
+		// POST: api/Accommodations
+		[ResponseType(typeof(Accommodation))]
+		public IHttpActionResult PostAccommodation(Accommodation accommodation)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (AccommodationExists(accommodation.Id))
+			{
+				return BadRequest("Accommodation exists.");
+			}
+
+
+			accommodation.Place = db.Places.Find(accommodation.Place.Id);
+			accommodation.AccommodationType = db.AccommodationTypes.Find(accommodation.AccommodationType.Id);
+			accommodation.Comments = new System.Collections.Generic.List<Comment>();
+
+			db.Accommodations.Add(accommodation);
+			db.SaveChanges();
+
+			return CreatedAtRoute("DefaultApi", new { id = accommodation.Id }, accommodation);
+		}
+
+		// DELETE: api/Accommodations/5
+		[ResponseType(typeof(Accommodation))]
+		public IHttpActionResult DeleteAccommodation(int id)
+		{
+			Accommodation accommodation = db.Accommodations.Find(id);
+			if (accommodation == null)
+			{
+				return NotFound();
+			}
+
+			db.Accommodations.Remove(accommodation);
+			db.SaveChanges();
+
+			return Ok(accommodation);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private bool AccommodationExists(int id)
+		{
+			return db.Accommodations.Count(e => e.Id == id) > 0;
+		}
+	}
 }
