2ad16d3eaac17df6e931d5fc07950a710fa6a786
Stefan <stefan_94@rocketmail.com>
2020-01-23 15:19:46 +0100

    Swagger and routes added.

--- BookingApp/Controllers/AccommodationsController.cs
+++ BookingApp/Controllers/AccommodationsController.cs
@@ -1,4 +1,5 @@
 ﻿using BookingApp.Models;
+using BookingApp.Resources;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
@@ -8,7 +9,8 @@ using System.Web.Http.Description;
 
 namespace BookingApp.Controllers
 {
-	public class AccommodationsController : ApiController
+    [RoutePrefix("api/Accommodations")]
+    public class AccommodationsController : ApiController
 	{
 		private IBAContext db;
 
@@ -22,22 +24,35 @@ namespace BookingApp.Controllers
 			db = context;
 		}
 
-		// GET: api/Accommodations
-		public IQueryable<Accommodation> GetAccommodations()
+        /// <summary>
+        /// Get all accommodations.
+        /// </summary>
+        /// <returns></returns>
+        [Route(Routes.GET_ACCOMMODATIONS)]
+        public IQueryable<Accommodation> GetAccommodations()
 		{
 			return db.Accommodations.Include(x => x.AccommodationType).Include(p => p.Place).Include(t => t.Rooms);
 		}
 
-		// GET: api/Accommodations
-		public IQueryable<Accommodation> GetPopularAccommodations()
-		{
-			return db.Accommodations.Include(x => x.AccommodationType).Include(p => p.Place).Include(t => t.Rooms).Where(x=>x.AverageGrade>4);
-		}
+        /// <summary>
+        /// Get popular accommodations.
+        /// Popular accommodations have average grade higher than 4.
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetPopularAccommodations")]
+        public IQueryable<Accommodation> GetPopularAccommodations()
+        {
+            return db.Accommodations.Include(x => x.AccommodationType).Include(p => p.Place).Include(t => t.Rooms).Where(x => x.AverageGrade > 4);
+        }
 
-
-		// GET: api/Accommodations/5
-		[ResponseType(typeof(Accommodation))]
-		public IHttpActionResult GetAccommodation(int id)
+        /// <summary>
+        /// Get accommodation by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(Accommodation))]
+        [Route("GetAccommodation")]
+        public IHttpActionResult GetAccommodation(int id)
 		{
 			Accommodation accommodation = db.Accommodations.Find(id);
 			if (accommodation == null)
@@ -48,9 +63,15 @@ namespace BookingApp.Controllers
 			return Ok(accommodation);
 		}
 
-		// PUT: api/Accommodations/5
+		/// <summary>
+        /// Update existing accommodation.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="accommodation"></param>
+        /// <returns></returns>
 		[ResponseType(typeof(void))]
-		public IHttpActionResult PutAccommodation(int id, Accommodation accommodation)
+        [Route("PutAccommodation")]
+        public IHttpActionResult PutAccommodation(int id, Accommodation accommodation)
 		{
 			if (!ModelState.IsValid)
 			{
@@ -82,10 +103,15 @@ namespace BookingApp.Controllers
 
 			return StatusCode(HttpStatusCode.NoContent);
 		}
-
-		// POST: api/Accommodations
+        
+        /// <summary>
+        /// Post new accommodation.
+        /// </summary>
+        /// <param name="accommodation"></param>
+        /// <returns></returns>
 		[ResponseType(typeof(Accommodation))]
-		public IHttpActionResult PostAccommodation(Accommodation accommodation)
+        [Route("PostAccommodation")]
+        public IHttpActionResult PostAccommodation(Accommodation accommodation)
 		{
 			if (!ModelState.IsValid)
 			{
@@ -108,9 +134,15 @@ namespace BookingApp.Controllers
 			return CreatedAtRoute("DefaultApi", new { id = accommodation.Id }, accommodation);
 		}
 
-		// DELETE: api/Accommodations/5
+
+        /// <summary>
+        /// Delete accommodation.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
 		[ResponseType(typeof(Accommodation))]
-		public IHttpActionResult DeleteAccommodation(int id)
+        [Route("DeleteAccommodation")]
+        public IHttpActionResult DeleteAccommodation(int id)
 		{
 			Accommodation accommodation = db.Accommodations.Find(id);
 			if (accommodation == null)
---
+++ BookingApp/Resources/Routes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingApp.Resources
+{
+    public class Routes
+    {
+        public const string GET_ACCOMMODATIONS = "GetAllAccommodations";
+        public const string GET_POPULAR_ACCOMMODATIONS = "GetPopularAccommodations";
+    }
+}
